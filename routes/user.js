cedf084826b3672954246cd45577025b897de70e
amitghosh <amit1999ghosh99@gmail.com>
2023-06-30 09:14:23 +0530

    remove params id

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,11 @@ const userController = require("../controllers/userController");
 const { verifyAndAuthorized,verifyToken,verifyAndAdmin } = require("../middleware/verifyToken");
 const router = require("express").Router();
 
-router.put("/:id",verifyAndAuthorized, userController.updateUser);
+router.put("/",verifyAndAuthorized, userController.updateUser);
 
-router.delete("/:id",verifyAndAuthorized, userController.deleteUser);
+router.delete("/",verifyAndAuthorized, userController.deleteUser);
 
-router.get("/:id",verifyAndAuthorized, userController.getUser);
+router.get("/",verifyAndAuthorized, userController.getUser);
 
 router.get("/",verifyAndAdmin, userController.getAllUser);
 
